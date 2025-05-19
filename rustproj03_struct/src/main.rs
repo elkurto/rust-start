968c37811bec822cc5e3395c5c436dfc63cc9bca
elkurto <kurt.buehner@gmail.com>
2025-05-19 16:43:33 -0400

    create, mutate, print struct

--- rustproj03_struct/src/main.rs
+++ rustproj03_struct/src/main.rs
@@ -1,3 +1,4 @@
+#[allow(dead_code)]
 #[derive(Debug)]
 struct Person {
     name: String,
@@ -17,16 +18,16 @@ struct Rect {
 fn main() {
 
     let jj =Person { name:String::from("jj"), age:18};
-    println!("jj = {:?}", jj);
+    println!("jj = {:#?}", jj);
 
 
     let mut rect = Rect{top: 20, left: 20, right: 30, bottom: 30};
-    println!("rect = {:?}", rect);
+    println!("rect = {:#?}", rect);
 
     rect.top =50;
     rect.left =50;
     rect.right =60;
     rect.bottom =60;
 
-    println!("rect = {:?}", rect);
+    println!("rect = {:#?}", rect);
 }
