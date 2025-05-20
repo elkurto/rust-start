26bf771d64694df03b2e85e993d64e0ebb504fe8
elkurto <kurt.buehner@gmail.com>
2025-05-20 09:47:14 -0400

    docs in example rustproj03_struct

--- rustproj03_struct/src/main.rs
+++ rustproj03_struct/src/main.rs
@@ -5,6 +5,7 @@ struct Person {
     age: u8,
 }
 
+#[allow(dead_code)]
 #[derive(Debug)]
 struct Rect {
     top: i64,
@@ -13,21 +14,36 @@ struct Rect {
     bottom: i64,
 }
 
-
-
 fn main() {
 
+    ex00_c_like_struct_person_and_rect();
+}
+
+fn ex00_c_like_struct_person_and_rect() {
+    println!("\nex00_c_like_struct_person_and_rect()");
     let jj =Person { name:String::from("jj"), age:18};
-    println!("jj = {:#?}", jj);
+    println!("  jj = {:?}", jj); // name
 
 
     let mut rect = Rect{top: 20, left: 20, right: 30, bottom: 30};
-    println!("rect = {:#?}", rect);
+    println!("  rect = {:?}", rect);
 
     rect.top =50;
     rect.left =50;
     rect.right =60;
     rect.bottom =60;
 
-    println!("rect = {:#?}", rect);
+    println!("  rect = {:#?}", rect);
+
+    //output:
+    // ex00_c_like_struct_person_and_rect()
+    //   jj = Person { name: "jj", age: 18 }
+    //   rect = Rect { top: 20, left: 20, right: 30, bottom: 30 }
+    //   rect = Rect {
+    //     top: 50,
+    //     left: 50,
+    //     right: 60,
+    //     bottom: 60,
+    //   }
+
 }
