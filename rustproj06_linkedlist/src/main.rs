9c5fea1e15ea9167389d20397477c541081fef6d
elkurto <kurt.buehner@gmail.com>
2025-05-23 15:31:39 -0400

    added example `rustc --explain E0382` to readme

--- rustproj06_linkedlist/src/main.rs
+++ rustproj06_linkedlist/src/main.rs
@@ -1,16 +1,24 @@
 use rustproj06_linkedlist::*;
 
+
 fn main() {
+    let mut n1 =NodeInt32::new(1);
+
+    n1.next = Some(Box::new(NodeInt32::new(2)));
+
+    println!("n1={} ", n1);  // output: n1=1,2,None
+
+    println!("n1.next.as_ref().unwrap ={}", n1.next.as_ref().unwrap());
+    // output:
+    //   n1=1,2,None
+    //   n1.next.as_ref().unwrap =2,None
+    // 
+
+
     // let mut list =ListInt32::new();
     //
     // list.push_i32(1);
     // list.push_i32(2);
     // list.push_i32(3);
 
-    let mut n1 =NodeInt32::new(1);
-    let n2 =NodeInt32::new(2);
-    n1.next = Some(Box::new(n2));
-
-
-    println!("n1={} ", n1);
 }
